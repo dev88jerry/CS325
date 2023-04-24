2cb7a2c536f31dc19fc79e2a4d7c22781b4932a8
Jerry <70291890+dev88jerry@users.noreply.github.com>
2023-04-24 12:55:02 -0400

    Create docker-stuff.sh

---
+++ VPN Lab/docker-stuff.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# some docker commands
+
+# show all containers
+docker container ls
+
+# connect - you can use tab to autocomplete the NAME
+docker exec -it NAME bash
+
+# stop 1 container
+docker container stop NAME
+
+# stop all containers
+docker stop $(docker ps -a -q)
